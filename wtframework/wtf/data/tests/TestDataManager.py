c55120404898d2f7b4b958e70b089f19aca6106f
David Lai <me@dlai0001.com>
2013-02-12 00:37:50 -0800

    unit test for no-env specified.

--- wtframework/wtf/data/tests/TestDataManager.py
+++ wtframework/wtf/data/tests/TestDataManager.py
@@ -16,6 +16,12 @@ class TestDataManager(unittest.TestCase):
         self.assertTrue(os.path.exists(file_path), \
                         "Expecting 'testdata.csv' to be under /data/testenv folder.")
 
+    def test_data_manager_returns_filepath_without_env(self):
+        file_path = DataManager().get_data_path("testdata1.csv")
+        self.assertTrue(os.path.exists(file_path), \
+                        "Expecting 'testdata1.csv' to be under /data/ folder.")
+
+
     def test_data_manager_throws_error_when_data_not_found(self):
         self.assertRaises(RuntimeError, DataManager().get_data_path, "testnodata.csv", "testenv")
         
