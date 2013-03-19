2ad8d5d01ee7af48bec9a7f294cc2cab73631fb8
David Lai <david@wiredrive.com>
2013-03-19 14:13:59 -0700

    clean up comments
    added gpl boilerplate

--- wtframework/wtf/tests/test_config_reader.py
+++ wtframework/wtf/tests/test_config_reader.py
@@ -14,11 +14,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with WTFramework.  If not, see <http://www.gnu.org/licenses/>.
 ##########################################################################
-'''
-Created on Dec 13, 2012
-
-@author: David Lai
-'''
 from wtframework.wtf.config import ConfigReader
 import unittest
 
--- wtframework/wtf/tests/test_timeout_manager.py
+++ wtframework/wtf/tests/test_timeout_manager.py
@@ -1,11 +1,21 @@
-'''
-Created on Mar 6, 2013
-
-@author: "David Lai"
-'''
+##########################################################################
+#This file is part of WTFramework. 
+#
+#    WTFramework is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    WTFramework is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with WTFramework.  If not, see <http://www.gnu.org/licenses/>.
+##########################################################################
+from wtframework.wtf.config import ConfigReader, TimeOutManager
 import unittest
-from wtframework.wtf.config import ConfigReader
-from wtframework.wtf.config import TimeOutManager
 
 
 class TestTimeOutManager(unittest.TestCase):
