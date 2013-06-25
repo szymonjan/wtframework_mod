1331525abb45cd506cf25553afdbb99f5290fe63
David Lai <me@dlai0001.com>
2013-06-25 02:09:23 -0700

    bump up version numbers

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from distutils.core import setup
 
 setup(
     name='wtframework',
-    version='0.2.13',
+    version='0.2.14',
     author='David Lai',
     author_email='david@wiredrive.com',
     packages=['wtframework',
