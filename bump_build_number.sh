c3d075a4f403079804519b753dbe01cbac1b0b63
David Lai <david@wiredrive.com>
2013-08-23 18:07:01 -0700

    bump version number script

---
+++ bump_build_number.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "subsituting: s/\d+\.\d+\.\d+/$1"
+
+sed s/$1/$2/ setup.py > setup.pie && mv setup.pie setup.py
+sed s/$1/$2/ wtframework/__init__.py > init.pie && mv init.pie wtframework/__init__.py
+sed s/$1/$2/ browser-plugins/chrome/wtf_page_mapper/manifest.json > manifest.jason && mv manifest.jason browser-plugins/chrome/wtf_page_mapper/manifest.json
