81bd24a55dca5a2117bc0e1d26cf7bfdf6d1fc7c
David Lai <david@wiredrive.com>
2014-02-26 11:50:22 -0800

    version number bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from distutils.core import setup
 
 setup(
     name='wtframework',
-    version='0.4.2',
+    version='0.4.3',
     author='David Lai',
     author_email='david@wiredrive.com',
     packages=['wtframework',
